ad67ac39b934fa151c6d056042d0f121f73132b9
WhyNotMrt <arthurmorisset@calvados.fr>
2022-03-07 12:06:43 +0100

    Complete menu functionnality

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,13 +4,15 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public string levelToLoad;
+    public GameObject settingsWindow;
     public void StartGameButton()
     {
         SceneManager.LoadScene(levelToLoad);
     }
     public void SettingsButton()
     {
-
+        gameObject.SetActive(false);
+        settingsWindow.SetActive(true);
     }
     public void QuitGameButton()
     {
--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,9 +1,80 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer am;
+    public Dropdown resolutionDropdown;
+    public GameObject mainMenu;
+
+    private Resolution[] resolutions;
+    private FullScreenMode fsm;
+
+    public void Start()
+    {
+        fsm = FullScreenMode.MaximizedWindow;
+        resolutions = Screen.resolutions.Select(r => new Resolution { width = r.width, height = r.height }).Distinct().ToArray();
+
+        int currentWidth = Screen.currentResolution.width;
+        int currentHeight = Screen.currentResolution.height;
+        List<string> options = new List<string>();
+
+        resolutionDropdown.ClearOptions();
+
+        int currentResolutionsIndex = 0;
+        int index = 0;
+        foreach(Resolution r in resolutions)
+        {
+            int width = r.width;
+            int height = r.height;
+            string option = width + "x" + height;
+            /*if (index == 0 || options[index - 1] != option)
+            {*/
+                options.Add(option);
+                if (width == currentWidth && height == currentHeight) currentResolutionsIndex = index;
+                index++;
+            /*}*/
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionsIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Back")) ReturnToMainMenu();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, fsm);
+    }
+
+    public void SetDisplay(int display)
+    {
+        switch (display)
+        {
+            case 0:
+                fsm = FullScreenMode.ExclusiveFullScreen;
+                break;
+            case 1:
+                fsm = FullScreenMode.FullScreenWindow;
+                break;
+            case 2:
+                fsm = FullScreenMode.MaximizedWindow;
+                break;
+            default:
+                fsm = FullScreenMode.MaximizedWindow;
+                break;
+        }
+        Screen.fullScreenMode = fsm;
+    }
 
     public void SetMusicVolume(float volume)
     {
@@ -15,8 +86,9 @@ public class SettingsMenu : MonoBehaviour
         am.SetFloat("Sound", volume);
     }
 
-    public void SetDisplay(string display)
+    public void ReturnToMainMenu()
     {
-        /*Screen.fullScreenMode(display);*/
+        mainMenu.SetActive(true);
+        gameObject.SetActive(false);
     }
 }
